83e69e10b39b6fcac4adead544ac145aed2de5d3
Yapii234 <yafi.233040113@mail.unpas.ac.id>
2025-02-13 14:43:37 +0700

    Mengerjakan Tugas PP1 Kelas D Hari Kamis

---
+++ src/Latihan1/Node.java
@@ -0,0 +1,29 @@
+package Latihan1;
+
+public class Node {
+		public int getNilai() {
+		return nilai;
+	}
+
+	public void setNilai(int nilai) {
+		this.nilai = nilai;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+		private int nilai;
+		private Node next;
+		
+		//inisialisasi Node
+		public Node (int nilai) {
+			this.nilai = nilai;
+		}
+	}
+
+
